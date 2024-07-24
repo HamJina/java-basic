a7ee3c7c330b7bc88c9aef0d4ba402e0fa2332d7
jina ham <hamjina61@gmail.com>
2024-07-24 15:00:00 +0900

    ㄹ0724: 객체지향 프로그래밍

---
+++ src/oop1/ValueObject.java
@@ -0,0 +1,13 @@
+package oop1;
+
+public class ValueObject {
+
+    //멤버변수
+    int value;
+
+    //value증가 메서드
+    void add() {
+        value++;
+        System.out.println("숫자 증가 value= " + value);
+    }
+}
